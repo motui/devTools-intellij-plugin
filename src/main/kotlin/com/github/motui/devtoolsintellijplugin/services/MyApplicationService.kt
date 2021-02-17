0249b868f8826182151da48e541ad641eb966b72
GitHub Action <action@github.com>
2021-02-17 02:47:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/motui/devtoolsintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.motui.devtoolsintellijplugin.services
+
+import com.github.motui.devtoolsintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
